22513118de1e553cd117bd2b34275e5a0efe6249
Ravi Vishwakarma <ravi_vishwakarma@Ravis-MBP.lan>
2024-09-14 19:18:38 +0530

    push method created in LinkedList

--- index.js
+++ index.js
@@ -18,7 +18,22 @@ class LinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+
+  // push method
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 const myLinkedList = new LinkedList(77);
-console.log(myLinkedList);
+
+console.log(myLinkedList.push(4));
