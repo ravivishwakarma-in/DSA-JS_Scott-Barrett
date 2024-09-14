a4a08d73ca05bd94a50af4fb1ba7fe27ad3e105c
Ravi Vishwakarma <ravi_vishwakarma@Ravis-MBP.lan>
2024-09-15 00:06:48 +0530

    unshift method created in LinkedList

--- index.js
+++ index.js
@@ -33,8 +33,10 @@ class LinkedList {
   // pop method
   pop() {
     if (!this.head) {
+      // if there is no node in the LinkedList
       return undefined;
     } else {
+      // if there are multiple nodes in the LinkedList
       let temp = this.head;
       let pre = this.head;
       while (temp.next) {
@@ -45,14 +47,31 @@ class LinkedList {
       this.tail.next = null;
       this.length--;
       if (this.length === 0) {
+        // if there is one node in the LinkedList
         this.head = null;
         this.tail = null;
       }
       return temp;
     }
   }
+
+  // unshift method
+  unshift(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
-const myLinkedList = new LinkedList();
-
+const myLinkedList = new LinkedList(7);
+console.log(myLinkedList.push(8));
 console.log(myLinkedList.pop());
+console.log(myLinkedList.unshift(6));
+console.log(myLinkedList);
